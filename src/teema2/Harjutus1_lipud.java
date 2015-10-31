fb6067f9ad6ad077a9f43e95aec62da9776577f3
Marju Pütsepp <mputsepp@klass2-13-l.intra.itcollege.ee>
2015-10-31 17:24:25 +0200

    Uued harjutused

--- src/teema2/Harjutus1_lipud.java
+++ src/teema2/Harjutus1_lipud.java
@@ -1,8 +1,60 @@
 package teema2;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
 /**
  * 1. Joonista eesti lipp kasutades kolme Rectangle
  * 2. Joonista Jaapani lipp kasutades Rectangle ja Circle
  */
-public class Harjutus1_lipud {
+public class Harjutus1_lipud extends Application{
+    Stage lava = new Stage();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        eestiLipp();
+        jaapaniLipp();
+    }
+
+    private void jaapaniLipp() {
+        Stage lava = new Stage();
+        Pane pane = new Pane();
+
+        Rectangle kast1 = new Rectangle(0,0, 600, 300);
+        kast1.setFill(Color.WHITE);
+
+        Circle ring = new Circle(300, 150, 100);
+        ring.setFill(Color.RED);
+
+        pane.getChildren().addAll(kast1, ring);
+
+        Scene stseen = new Scene(pane, 600, 300);
+        lava.setScene(stseen);
+        lava.show();
+    }
+
+    private void eestiLipp() {
+        Stage lava = new Stage();
+        Pane pane = new Pane();
+
+        Rectangle sinine = new Rectangle(0,0, 900, 300);
+        sinine.setFill(Color.BLUE);
+
+        Rectangle must = new Rectangle(0,300, 900, 300);
+        must.setFill(Color.BLACK);
+
+        Rectangle valge = new Rectangle(0,600, 900, 300);
+        valge.setFill(Color.WHITE);
+
+        pane.getChildren().addAll(sinine, must, valge);
+
+        Scene stseen = new Scene(pane, 900, 900);
+        lava.setScene(stseen);
+        lava.show();
+    }
 }
--- src/teema2/Harjutus3_logisisse.java
+++ src/teema2/Harjutus3_logisisse.java
@@ -1,5 +1,6 @@
 package teema2;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -17,5 +18,30 @@ import javafx.stage.Stage;
  * 3. Ebaõnnestunud katse näitab kasutajale errorit
  * 4. Õnnestunud katse puhul vaheta pilt uue vastu (kasvõi roheline ring), .
  */
-public class Harjutus3_logisisse {
+public class Harjutus3_logisisse extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        VBox vbox = new VBox();
+
+        Label l = new Label("Kasutajanimi");
+        TextField kasutajaInput = new TextField();
+        kasutajaInput.setPromptText("kasutajanimi");
+
+        Label l2 = new Label("Parool");
+        PasswordField parool = new PasswordField();
+        parool.setPromptText("parool");
+
+        Button nupp = new Button("Logi sisse");
+        nupp.setOnAction(event -> {
+                    System.out.println("Logisid sisse");
+                }
+        );
+
+        vbox.getChildren().addAll(l, kasutajaInput, l2, parool, nupp);
+
+        Scene stseen = new Scene(vbox);
+        primaryStage.setScene(stseen);
+        primaryStage.show();
+    }
 }
